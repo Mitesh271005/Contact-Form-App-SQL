562ade9cf0afdc6c1dd9b9bd5684b66dc180b3ea
Mitesh271005 <miteshghadiya777@gmail.com>
2025-07-18 16:28:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 const app = express();
-const PORT = 8000;
+const PORT = 4000;
 
 // MySQL connection
 const db = mysql.createConnection({
